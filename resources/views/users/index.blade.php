1774bc47ea1ace5a5b7493135478a5ac444eb49a
rafapoldo <rafaelsteffens@Rafaels-MacBook-Air.local>
2023-03-06 21:19:35 -0300

    aulas 8 a 10

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -8,5 +8,14 @@
 </head>
 <body>
    <h1>Hello World</h1>
+   <ul>
+      @foreach($users as $user)
+         <li>
+            {{ $user->name }} - 
+            {{ $user->email }}
+         </li>
+      @endforeach
+   </ul>
+
 </body>
 </html>
